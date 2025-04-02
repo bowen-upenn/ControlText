5f0f80990313f2db3d4c06986d174b9f2a4b9c54
bowen-upenn <bwjiang@seas.upenn.edu>
2025-04-02 01:38:47 -0400

    fix NED calculation in function get_ld

--- eval/eval_ocr.sh
+++ eval/eval_ocr.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=7
 python eval/eval_dgocr.py \
-        --img_dir ./eval/laion_controltext \
-        --json_path /tmp/datasets/AnyWord-3M/AnyText-Benchmark/benchmark/laion_word/test1k.json \
-        --glyph_path ./Rethinking-Text-Segmentation/log/images/output/anytext_benchmark/laion_word
+        --img_dir ./eval/wukong_ablation_gly_lines \
+        --json_path /tmp/datasets/AnyWord-3M/AnyText-Benchmark/benchmark/wukong_word/test1k.json \
+        --glyph_path ./Rethinking-Text-Segmentation/log/images/output/anytext_benchmark/wukong_word
+
+# #!/bin/bash
+# export CUDA_VISIBLE_DEVICES=7
+# python eval/eval_dgocr.py \
+#         --img_dir ./eval/textdiffuser_laion_generated_gly_lines \
+#         --json_path /tmp/datasets/AnyWord-3M/AnyText-Benchmark/benchmark/laion_word/test1k.json \
+#         --glyph_path ./Rethinking-Text-Segmentation/log/images/output/anytext_benchmark/laion_word
+
+# #!/bin/bash
+# export CUDA_VISIBLE_DEVICES=7
+# python eval/eval_dgocr.py \
+#         --img_dir ./eval/glyphcontrol_laion_generated_gly_lines \
+#         --json_path /tmp/datasets/AnyWord-3M/AnyText-Benchmark/benchmark/laion_word/test1k.json \
+#         --glyph_path ./Rethinking-Text-Segmentation/log/images/output/anytext_benchmark/laion_word
+
+# #!/bin/bash
+# export CUDA_VISIBLE_DEVICES=7
+# python eval/eval_dgocr.py \
+#         --img_dir ./eval/controlnet_wukong_generated_gly_lines \
+#         --json_path /tmp/datasets/AnyWord-3M/AnyText-Benchmark/benchmark/wukong_word/test1k.json \
+#         --glyph_path ./Rethinking-Text-Segmentation/log/images/output/anytext_benchmark/wukong_word
